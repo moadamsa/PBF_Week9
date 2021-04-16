63984c40fef1195b3a5a75bfe17c66990e21f367
MOCHAMMAD ADAM'S ARZAQI <81493782+moadamsa@users.noreply.github.com>
2021-04-16 20:44:42 +0700

    Add files via upload

---
+++ PBF_Week9-main/src/App.js
@@ -0,0 +1,21 @@
+// import logo from './logo.svg';
+import "./App.css";
+import React, { Component } from "react";
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+import List from "./list/List";
+import Login from "./login/login";
+import Test from "./test/Test";
+import Hello from "../src/component/HelloComponent";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Hello />
+        {/* <Login /> */}
+      </div>
+    );
+  }
+}
+export default App;
